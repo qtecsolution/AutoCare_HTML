3885a999ba4efb5fd2450540fe4e5893129546d3
Jahir <supersimple.jahir@gmail.com>
2024-03-18 10:19:17 +0600

    AutoCare file setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    "./src/**/*.{html,js}",
+    "*.{html,js}"
+    ],
+  content: [
+    "./src/**/*.{html,js}",
+    "*.{html,js}"
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
