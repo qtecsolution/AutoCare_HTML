6229a3886cb334f043184688549b552b257c6cb1
superSimpleJahir <supersimple.jahir@gmail.com>
2024-03-29 10:28:12 +0600

    [Done] Just for you section responsive

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -79,12 +79,16 @@ var swiper3 = new Swiper(".mySwiper3", {
       spaceBetween: 20,
     },
     768: {
-      slidesPerView: 3,
-      spaceBetween: 40,
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
     1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1280: {
       slidesPerView: 4,
-      spaceBetween: 50,
+      spaceBetween: 20,
     },
   },
 });
