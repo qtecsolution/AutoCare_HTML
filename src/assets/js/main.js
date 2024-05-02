751c8691f92c0b7bf27ec470ed11b002da927d35
Jahir <supersimple.jahir@gmail.com>
2024-05-02 16:06:00 +0600

    [Done] daterangepicker

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -117,3 +117,23 @@ $("#lightSlider").lightSlider({
   slideMargin: 0,
   thumbItem: 5,
 });
+
+
+$(function () {
+  $('input[name="datefilter"]').daterangepicker({
+    autoUpdateInput: false,
+    locale: {
+      cancelLabel: "Clear",
+    },
+  });
+
+  $('input[name="datefilter"]').on("apply.daterangepicker", function (ev, picker) {
+    $(this).val(
+      picker.startDate.format("MM/DD/YYYY") + " - " + picker.endDate.format("MM/DD/YYYY")
+    );
+  });
+
+  $('input[name="datefilter"]').on("cancel.daterangepicker", function (ev, picker) {
+    $(this).val("");
+  });
+});
