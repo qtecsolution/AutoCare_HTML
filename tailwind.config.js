b5551f1b2b1f6e01b20fac304b0838fdd3b3349f
kawsar ahmed <kawsar.qtec@gmail.com>
2024-05-12 17:28:33 +0600

    fixed container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
+
   purge: ["./src/**/*.{html,js}", "*.{html,js}"],
   content: ["./src/**/*.{html,js}", "*.{html,js}"],
   theme: {
+   
     fontFamily: {
       inter: ["Inter", "sans-serif"],
       alata: ["Alata", "sans-serif"],
